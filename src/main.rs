6cbb25d6c385e5caa32856711c1947c14b050c5b
RockBanzai <hrocca76@gmail.com>
2023-03-24 19:09:25 -0300

    feat: add new endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpResponse, get, web};
+
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+            .service(index)
+    });
+
+    server.bind("127.0.0.1:8000")?.run().await
 }
